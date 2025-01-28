8ec9a5e493079ba2e1f4d9d890059cc9a919b927
alijah-tectonics <alijahlemay12@gmail.com>
2025-01-27 17:59:30 -0800

    db edits

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
       // useCreateIndex: true,
-      setupMiddlewares: true,
+      // setupMiddlewares: true,
     })
 
     console.log('MongoDB connected')
